101387c61192d5b50b56272043c19c9db9bd2448
Adam Gausmann <agausmann@fastmail.com>
2022-05-31 23:52:18 -0500

    Fix feature cfg

--- polybius/src/lib.rs
+++ polybius/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![feature(generic_const_exprs)]
-#![cfg_attr(feature = "planck_rev2", feature(asm_experimental_arch))]
+#![cfg_attr(target_arch = "avr", feature(asm_experimental_arch))]
 
 pub mod backlight;
 pub mod diodes;
